6c7ee0882229636afb8ef5104ccd58a0c7352900
최우성/Inapp서비스개발팀/원스토어 <vov1252@naver.com>
2023-12-29 16:02:37 +0900

    [update] Ktor 추가

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -14,7 +14,7 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -28,7 +28,9 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                //put your multiplatform dependencies here
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.serialization)
+                implementation(libs.ktor.client.serialization.json)
             }
         }
         val commonTest by getting {
