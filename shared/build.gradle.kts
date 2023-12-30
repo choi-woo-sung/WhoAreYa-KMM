5e16390524632d481ff7f489162a98f52e89d9b0
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-12-30 14:10:14 +0900

    Add cocoapods config

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinCocoapods)
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    targetHierarchy.default()
+    applyDefaultHierarchyTemplate()
 
     androidTarget {
         compilations.all {
@@ -15,13 +16,18 @@ kotlin {
         }
     }
 
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        version = "1.0.0"
+        ios.deploymentTarget = "16.0"
+        podfile = project.file("../whoAreYaIosApp/Podfile")
+        framework {
             baseName = "shared"
+            isStatic = true
         }
     }
 
---
+++ whoAreYaIosApp/Pods/Target Support Files/Pods-whoAreYaIosApp/Pods-whoAreYaIosApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_whoAreYaIosApp : NSObject
+@end
+@implementation PodsDummy_Pods_whoAreYaIosApp
+@end
